4498ce815ce5041afad3eccd8b2a83baa5460b83
Aymeric <aymeric.nunge@gmail.com>
2024-01-05 21:29:12 +0100

    yeah

--- src/pattgen_backend/src/lib.rs
+++ src/pattgen_backend/src/lib.rs
@@ -1,35 +1,12 @@
 use ic_cdk::api::management_canister::http_request::{
     http_request, CanisterHttpRequestArgument, HttpMethod, HttpHeader,
 };
+use base64::encode;
 
-const API_KEY: &str = "...";
-
-// #[ic_cdk::update]
-// async fn start_gen(prompt: String) -> String {
-//     let request = CanisterHttpRequestArgument {
-//         url: String::from("https://httpbin.org/get"),
-//         method: HttpMethod::GET,
-//         body: None,               //optional for request
-//         max_response_bytes: None, //optional for request
-//         transform: None,          //optional for request
-//         headers: vec![],
-//     };
-
-//     match http_request(request).await {
-//         Ok((response,)) => {
-//             let body = String::from_utf8(response.body).unwrap();
-//             return body;
-//         }
-//         Err((r, m)) => {
-//             let message =
-//                 format!("The http_request resulted into error. RejectionCode: {r:?}, Error: {m}");
-//             return message;
-//         }
-//     }
-// }
+const API_KEY: &str = "";
 
 #[ic_cdk::update]
-async fn start_gen(prompt: String) -> String {
+async fn audio_start_gen(prompt: String) -> String {
     // Constructing JSON string manually
     let json_string = format!(
         "{{ \"version\": \"b05b1dff1d8c6dc63d14b0cdb42135378dcb87f6373b0d3d341ede46e59e2b38\", \"input\": {{ \"model_version\": \"stereo-melody-large\", \"prompt\": \"{}\" }} }}",
@@ -74,7 +51,7 @@ async fn start_gen(prompt: String) -> String {
 
 
 #[ic_cdk::update]
-async fn check_gen(id: String) -> String {
+async fn audio_check_gen(id: String) -> String {
     let url = format!("https://api.replicate.com/v1/predictions/{}", id);
 
     let request_headers = vec![
@@ -102,6 +79,156 @@ async fn check_gen(id: String) -> String {
         Err((r, m)) => {
             let message = format!("API_ERR. The http_request resulted in an error. RejectionCode: {r:?}, Error: {m}");
             return message;
+        }
+    }
+}
+
+#[ic_cdk::update]
+async fn image_start_gen(prompt: String) -> String {
+    // Constructing the JSON string manually to match the new payload structure
+    let json_string = format!(
+        r#"{{ 
+            "version": "ac732df83cea7fff18b8472768c88ad041fa750ff7682a21affe81863cbe77e4",
+            "input": {{
+                "width": 512,
+                "height": 512,
+                "prompt": "{}",
+                "scheduler": "K_EULER",
+                "num_outputs": 1,
+                "guidance_scale": 7.5,
+                "num_inference_steps": 50
+            }}
+        }}"#,
+        prompt
+    );
+    let request_body: Option<Vec<u8>> = Some(json_string.into_bytes());
+
+    let request_headers = vec![
+        HttpHeader {
+            name: "Content-Type".to_string(),
+            value: "application/json".to_string(),
+        },
+        HttpHeader {
+            name: "Authorization".to_string(),
+            value: format!("Token {}", API_KEY),
+        },
+        // Add other headers here as needed
+    ];
+
+    let request = CanisterHttpRequestArgument {
+        url: String::from("https://api.replicate.com/v1/predictions"),
+        method: HttpMethod::POST,
+        body: request_body,
+        max_response_bytes: None,
+        transform: None,
+        headers: request_headers,
+    };
+
+    match http_request(request).await {
+        Ok((response,)) => {
+            let body = String::from_utf8(response.body).unwrap();
+            return body;
+        }
+        Err((r, m)) => {
+            let message = 
+                format!("API_ERR. The http_request resulted in an error. RejectionCode: {r:?}, Error: {m}");
+            return message;
+        }
+    }
+}
+
+#[ic_cdk::update]
+async fn image_check_gen(get_url: String) -> String {
+    let request_headers = vec![
+        HttpHeader {
+            name: "Authorization".to_string(),
+            value: format!("Token {}", API_KEY),
+        },
+        // Add other headers here as needed
+    ];
+
+    let request = CanisterHttpRequestArgument {
+        url: get_url,
+        method: HttpMethod::GET,
+        body: None, // No body needed for GET request
+        max_response_bytes: None,
+        transform: None,
+        headers: request_headers,
+    };
+
+    match http_request(request).await {
+        Ok((response,)) => {
+            let body = String::from_utf8(response.body).unwrap();
+            return body;
+        }
+        Err((r, m)) => {
+            let message = 
+                format!("API_ERR. The http_request resulted in an error. RejectionCode: {r:?}, Error: {m}");
+            return message;
+        }
+    }
+}
+
+#[ic_cdk::update]
+async fn fetch_image_as_base64(get_url: String) -> String {
+    let request_headers = vec![
+        HttpHeader {
+            name: "Authorization".to_string(),
+            value: format!("Token {}", API_KEY),
+        },
+        // Add other headers here as needed
+    ];
+
+    let request = CanisterHttpRequestArgument {
+        url: get_url,
+        method: HttpMethod::GET,
+        body: None, // No body needed for GET request
+        max_response_bytes: None,
+        transform: None,
+        headers: request_headers,
+    };
+
+    match http_request(request).await {
+        Ok((response,)) => {
+            // Convert the response body (image data) to a Base64 string
+            let base64_image = encode(response.body);
+            return format!("data:image/png;base64,{}", base64_image);
+        }
+        Err((r, m)) => {
+            let error_message = 
+                format!("API_ERR. Failed to fetch image. RejectionCode: {r:?}, Error: {m}");
+            return error_message;
+        }
+    }
+}
+
+#[ic_cdk::update]
+async fn fetch_audio_raw(get_url: String) -> Vec<u8> {
+    let request_headers = vec![
+        HttpHeader {
+            name: "Authorization".to_string(),
+            value: format!("Token {}", API_KEY),
+        },
+        // Add other headers here as needed
+    ];
+
+    let request = CanisterHttpRequestArgument {
+        url: get_url,
+        method: HttpMethod::GET,
+        body: None, // No body needed for GET request
+        max_response_bytes: None,
+        transform: None,
+        headers: request_headers,
+    };
+
+    match http_request(request).await {
+        Ok((response,)) => {
+            response.body
+        }
+        Err((r, m)) => {
+            // Log the error or handle it as needed
+            ic_cdk::api::print(format!("API_ERR. Failed to fetch audio. RejectionCode: {r:?}, Error: {m}"));
+            Vec::new() // Return an empty Vec<u8>
         }
     }
 }
--- src/pattgen_frontend/src/index.js
+++ src/pattgen_frontend/src/index.js
@@ -1,61 +1,185 @@
-// function sendKeywords(keywords) {
-//     console.log("Sending keywords:", keywords);
-//     // Implement AJAX call or similar to send data to backend
-//     // Navigate to the next page after sending data
-// }
-
-// function navigate(direction) {
-//     console.log("Navigating", direction);
-//     // Implement logic to navigate to the previous or next page
-// }
-
 import { pattgen_backend } from "../../declarations/pattgen_backend";
 
-async function generate() {
-    const res = await pattgen_backend.start_gen("shitty flute heavy metal");
+async function setPlayerBackground(url) {
+    // no need
+    // const base64 = await pattgen_backend.fetch_image_as_base64(url);
 
-    // Check if the response contains 'API_ERR'
-    if (res.includes('API_ERR')) {
-        console.error("Error in API response:", res);
-        return;
-    }
+    const player = document.getElementById('player');
+    player.style.backgroundImage = `url(${url})`;
+    player.style.backgroundSize = '100% 100%';
+    player.style.animation = 'none';
+    player.textContent = '';
+}
+
+async function createAudioPlayer(audioUrl) {
+    // Create an audio element
+    const audioPlayer = document.createElement('audio');
+    audioPlayer.controls = true; // Add controls like play, pause
+    audioPlayer.style.display = 'block'; // Ensure it's visible
+    audioPlayer.src = audioUrl;
+
+    // Append the audio player to the div with id 'player'
+    const playerDiv = document.getElementById('player');
+    playerDiv.appendChild(audioPlayer);
+}
+
+async function generate_all() {
+    // set testButton disabled
+    document.getElementById('testButton').disabled = true;
+
+
+    const player = document.getElementById('player');
+    player.style.animation = 'gradientBG 5s ease infinite';
+    player.style.backgroundSize = '400% 400%';
 
     try {
-        // Parse the JSON response
-        const jsonRes = JSON.parse(res);
+        const promises = [
+            generate_audio("slow arctic death metal funky", (txt) => {
+                document.getElementById('player_txt').textContent = txt;
+            }),
+            generate_image("slow arctic death metal funky")
+        ];
 
-        // Extract the 'id' field
-        const id = jsonRes.id;
-        console.log("Extracted ID:", id);
+        const [audio_url, image_url] = await Promise.all(promises);
 
-        const checkStatus = async () => {
-            const checkRes = await pattgen_backend.check_gen(id);
+        console.log("audio_url:", audio_url);
+        console.log("image_url:", image_url);
 
-            // Check for API_ERR in the check_gen response
-            if (checkRes.includes('API_ERR')) {
-                console.error("Error in check_gen API response:", checkRes);
-                return;
-            }
+        const promisesEnd = [
+            setPlayerBackground(image_url),
+            createAudioPlayer(audio_url)
+        ];
 
-            // Parse the check_gen JSON response
-            const checkJsonRes = JSON.parse(checkRes);
+        await Promise.all(promisesEnd);
 
-            // Check if the generation process is complete and successful
-            if (checkJsonRes.status === "succeeded") {
-                const outputFile = checkJsonRes.output;
-                console.log("Generation succeeded. Output WAV file URL:", outputFile);
-                clearInterval(intervalId); // Stop checking
-            } else {
-                console.log("Generation is still in progress. Current status:", checkJsonRes.status);
-            }
-        };
-
-        // Start checking every 5 second
-        const intervalId = setInterval(checkStatus, 5000);
-
+        document.getElementById('testButton').disabled = false;
     } catch (e) {
-        console.error("Error processing the response:", e);
+        document.getElementById('testButton').disabled = false;
+        console.error("Error generating content:", e);
     }
 }
 
-document.getElementById('testButton').addEventListener('click', generate);
+
+async function generate_audio(prompt, progressCb) {
+    return new Promise(async (resolve, reject) => {
+        //
+        progressCb("Starting generation ...");
+        const res = await pattgen_backend.audio_start_gen(prompt);
+
+
+        if (res.includes('API_ERR')) {
+            console.error("Error in API response:", res);
+            progressCb("Error :(");
+            reject("Error in API response");
+            return;
+        }
+
+        try {
+            const jsonRes = JSON.parse(res);
+            const id = jsonRes.id;
+            console.log("Process ID:", id);
+            progressCb("In Queue ...");
+
+            let checking = false;
+            let intervalId;
+
+            const checkStatus = async () => {
+                if (checking) {
+                    return;
+                }
+
+                checking = true;
+                const checkRes = await pattgen_backend.audio_check_gen(id);
+                checking = false;
+
+                if (checkRes.includes('API_ERR')) {
+                    console.error("Error in check_gen API response:", checkRes);
+                    clearInterval(intervalId);
+                    progressCb("Error :(");
+                    reject("Error in check_gen API response");
+                    return;
+                }
+
+                const checkJsonRes = JSON.parse(checkRes);
+
+                if (checkJsonRes.status === "succeeded") {
+                    const outputFile = checkJsonRes.output;
+                    console.log("Generation succeeded. Output WAV file URL:", outputFile);
+                    clearInterval(intervalId);
+                    progressCb("Post-processing ...");
+                    resolve(outputFile);  // Resolving the promise with the outputFile URL
+                } else {
+                    progressCb("Processing ...");
+                    console.log("Audio Generation is still in progress. Current status:", checkJsonRes.status);
+                }
+            };
+
+            intervalId = setInterval(checkStatus, 1000);
+
+        } catch (e) {
+            console.error("Error processing the response:", e);
+            progressCb("Error :(");
+            reject(e.message);
+        }
+    });
+}
+
+async function generate_image(prompt) {
+    return new Promise(async (resolve, reject) => {
+        const res = await pattgen_backend.image_start_gen(prompt);
+
+        if (res.includes('API_ERR')) {
+            console.error("Error in API response:", res);
+            reject("Error in API response");
+            return;
+        }
+
+        console.log("IMG Response:", res);
+
+        try {
+            // Parse the JSON response
+            const jsonRes = JSON.parse(res);
+            const getURL = jsonRes.urls.get;
+            console.log("Get URL:", getURL);
+
+            let checking = false;
+            let intervalId;
+
+            const checkStatus = async () => {
+                if (checking) {
+                    return;
+                }
+
+                checking = true;
+                const checkRes = await pattgen_backend.image_check_gen(getURL);
+                checking = false;
+
+                if (checkRes.includes('API_ERR')) {
+                    console.error("Error in check_gen API response:", checkRes);
+                    clearInterval(intervalId);
+                    reject("Error in check_gen API response");
+                    return;
+                }
+
+                const checkJsonRes = JSON.parse(checkRes);
+
+                if (checkJsonRes.status === "succeeded") {
+                    const outputFile = checkJsonRes.output[0];
+                    console.log("Image generation succeeded. Output image URL:", outputFile);
+                    clearInterval(intervalId);
+                    resolve(outputFile);  // Resolving the promise with the outputFile URL
+                } else {
+                    console.log("Image Generation is still in progress. Current status:", checkJsonRes.status);
+                }
+            };
+
+            intervalId = setInterval(checkStatus, 1000);
+
+        } catch (e) {
+            console.error("Error parsing JSON response:", e);
+            reject("Error parsing JSON response");
+        }
+    });
+}
+
+document.getElementById('testButton').addEventListener('click', generate_all);
